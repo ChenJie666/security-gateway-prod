39a624b79de4935e4aaf5264ec8b94cd31c2b9ba
cj <792965772@qq.com>
2020-10-23 16:53:22 +0800

    添加映射信息，删除权限

--- src/main/java/com/iotmars/GatewayApplication.java
+++ src/main/java/com/iotmars/GatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
 /**
  * @author Administrator
  */
